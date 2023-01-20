7fae7f9b9a52e772836bd0d1a8ae01597483ee58
Jake Lazaroff <jake@clay3d.io>
2023-01-20 11:45:18 -0500

    Set filename when opening and saving file

--- clay/__init__.py
+++ clay/__init__.py
@@ -11,9 +11,6 @@ import bpy
 import requests
 
 
-api_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImQwNTU5YzU5MDgzZDc3YWI2NDUxOThiNTIxZmM4ZmVmZmVlZmJkNjIiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiSmFrZSBMYXphcm9mZiIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS0vQUZkWnVjcXF6T01ETVZQZHMza3FKVTNnUnVWc1ZRU1RVNEkyeWJHWXFCenQ2TG5WRnRLZm1OYWh6Q0RqeGZteS1jQXo4NWY2SXpKRWpFNkV1RE0zZWVhRVd1SVo3clJtb2VRb2QtS2NYNUo0X0ZuZU9lc1pGeXAwSzFEdHpCb0tuNkoyVVhURnlBR2xtTC1DZ0VyaHVienNCOTNSZ2c2SHgyNzFnZC1aaUdRaUdHaUVuaDZIM2tuVFJUdzRwWHVRekdXaXpFMDhFWkxoSXRxTVFqWk51WXBzMkV4T0FRRTZQSWJ5ZlBWeWNGU2hJQUlYTTVsSWJIV19MU2xtREM3RGhKd3FULTNqRFJDdzR0T20xbm51Z2xDeHIwZ3U1VXV4UVNZTEtJRzNlUGIxTU9QWXZqZmhTaUh6M1p1aTI5MWNkeGVGVEJmbjFPQVBmYWdiOGhyNUZ6c0o4Rm1HcTJ3Vmc2dDZWZTR0M2xvcFVMNG5OTEhGRzlWYVRxVmNxSktMeTNucGRER1lCQVI3Sk5XWEZYQlhqTnJ2ZnM5T1J0MURfR19pMVBjZjFQb1l2d0ZZWkVfakZ1aVI5VXJIQ3Uzc1MwMFVjOVpRM1lMZnZmV1AzOERGZUd5WTI4Q204R2JnNHVNZzBFOFpzRVVEc3pHc2J5UVZNWGxQbnFqY3dGVUJtcTlEdkVvWHdremZmRUJKVjFSUWs3WjNLcnBSdGNxUU1lRjF5MFlwSlRsT3FwNXppMFhYMnp0b1c4Ukx0YXd0RUpUUEFqWUVkQWVBU210RHVIU1VkZ3JidG5UUjBQRzV2bi1kazA1RXJ3eVBBSHZ5UXpJU2NUZmJKdXlaNGZUbDE5dUFoUFB0TlA5V1Ixc3ZubjBiRVhRVDRHMGxBNFdxdEVRTXlIQ2FiWHFqeFNNZkt3dkVySS1kNVpMZVlpdUFZQ003dDFGRzl5TmlvMXRKOUhkQjZ4MzVvVWI1SjQ0OGtZcXBqLTR2ZktlVGdJNE96a1NMa3lzTktpeHFEZkxYQUFJeFhXdEdELXZGPXM5Ni1jIiwiaXNzIjoiaHR0cHM6Ly9zZWN1cmV0b2tlbi5nb29nbGUuY29tL2NsYXkzZC02ZmVmMiIsImF1ZCI6ImNsYXkzZC02ZmVmMiIsImF1dGhfdGltZSI6MTY3Mzk5MDAxMSwidXNlcl9pZCI6InBINkpaTTlXdU9ONm1MVUl5Y2pJSlVhMXJEVTIiLCJzdWIiOiJwSDZKWk05V3VPTjZtTFVJeWNqSUpVYTFyRFUyIiwiaWF0IjoxNjc0MTY3Mjk3LCJleHAiOjE2NzQxNzA4OTcsImVtYWlsIjoiamFrZUBjbGF5M2QuaW8iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJnb29nbGUuY29tIjpbIjExNTMwMTk5ODMxODE1NTAwODEyMiJdLCJlbWFpbCI6WyJqYWtlQGNsYXkzZC5pbyJdfSwic2lnbl9pbl9wcm92aWRlciI6Imdvb2dsZS5jb20ifX0.AX6A19x323d-Rl5U3RSc8eOzesGLKsh9kJM7SCsN-2rfQhc6UtOUu0-seXzrSdWg_R3S2zjjb4uxRtxDr0q82hh4zhvDv8NrMV_UBPl0ohkquovQbV0R0f2_6DwsBt8zy3rxiyp1H22Fsv2a6S7WL1Yx3TTt-L7bTCQcJRPgOazDruXXAOsbebk_pfILHelYlD3ahMLY96K2gjstD3mKMllmtfXc2KlGmiIMd9J4ju42GI8_XvixFweiWteppQztgb8RUqDUi9oMXdV-RP1JtPUPWnBLzvYCAYa0hvYQvr_G0j5fDP0DMgIwRBZ80CcRa2donNcr-hDXwFXRTNIgLw"
-
-
 class Preferences(bpy.types.AddonPreferences):
     bl_idname = __name__
 
@@ -28,33 +25,42 @@ class Preferences(bpy.types.AddonPreferences):
         layout.prop(self, "api_key")
 
 
-class ClayExportModalOperator(bpy.types.Operator):
-    bl_idname = "clay.export_modal"
+class SceneProperties(bpy.types.PropertyGroup):
+    file_name: bpy.props.StringProperty(
+        name="File name",
+        default="",
+        maxlen=1024,
+    )
+
+
+class ExportOperator(bpy.types.Operator):
+    bl_idname = "clay.export"
     bl_label = "Export to Clay"
 
-    filename: bpy.props.StringProperty(name="Filename")
-
     def execute(self, context):
-        filename = bpy.path.ensure_ext(self.filename or "Untitled", ".glb")
+        clay = context.scene.clay
 
-        with tempfile.TemporaryDirectory() as folder:
-            filepath = os.path.join(folder, filename)
-            bpy.ops.export_scene.gltf(filepath=filepath, export_format="GLB")
+        file_name = bpy.path.ensure_ext(clay.file_name or "Untitled", ".glb")
+        print(file_name)
 
-            with open(filepath, mode="rb") as file:
-                url = "https://api.localhost.clay3d.io/v1/workspaces/e816747e-81ca-4788-bf92-3b9a56215c0b/files"
-                r = requests.post(
-                    url,
-                    verify=False,
-                    headers={"authorization": "Bearer " + api_token},
-                    files={filename: file},
-                )
-                print(r.text)
+        # with tempfile.TemporaryDirectory() as folder:
+        #     filepath = os.path.join(folder, filename)
+        #     bpy.ops.export_scene.gltf(filepath=filepath, export_format="GLB")
+
+        #     with open(filepath, mode="rb") as file:
+        #         url = "https://api.localhost.clay3d.io/v1/workspaces/e816747e-81ca-4788-bf92-3b9a56215c0b/files"
+        #         r = requests.post(
+        #             url,
+        #             verify=False,
+        #             headers={"authorization": "Bearer " + api_token},
+        #             files={filename: file},
+        #         )
+        #         print(r.text)
         return {"FINISHED"}
 
-    def invoke(self, context, event):
-        self.filename = bpy.path.display_name(context.blend_data.filepath, has_ext=True)
-        return context.window_manager.invoke_props_dialog(self)
+    # def invoke(self, context, event):
+    #     self.filename = bpy.path.display_name(context.blend_data.filepath, has_ext=True)
+    #     return context.window_manager.invoke_props_dialog(self)
 
 
 class ClayPanel(bpy.types.Panel):
@@ -68,29 +74,44 @@ class ClayPanel(bpy.types.Panel):
 
     def draw(self, context):
         # prefs = context.preferences.addons[__name__].preferences
-        print(context.scene)
-        self.layout.label(text="Hello,")
-        self.layout.prop(prefs, "api_key")
-        self.layout.label(text="world!")
+        # self.layout.label(text="Export")
+        self.layout.prop(context.scene.clay, "file_name")
+        self.layout.operator(ExportOperator.bl_idname, text="Export to Clay")
 
 
-def open_export_modal(self, context):
-    self.layout.operator(ClayExportModalOperator.bl_idname, text="Export to Clay")
+# def open_export_modal(self, context):
+#     self.layout.operator(ClayExportModalOperator.bl_idname, text="Export to Clay")
+
+
+@bpy.app.handlers.persistent
+def initialize_file_name(scene):
+    if bpy.context.scene.clay.file_name != "":
+        return
+
+    basename = bpy.path.basename(bpy.context.blend_data.filepath)
+    bpy.context.scene.clay.file_name = os.path.splitext(basename)[0]
 
 
 def register():
     bpy.utils.register_class(Preferences)
+    bpy.utils.register_class(SceneProperties)
     bpy.utils.register_class(ClayPanel)
-    bpy.utils.register_class(ClayExportModalOperator)
-    bpy.types.TOPBAR_MT_file.append(open_export_modal)
+    bpy.utils.register_class(ExportOperator)
+    # bpy.types.TOPBAR_MT_file.append(open_export_modal)
+    bpy.types.Scene.clay = bpy.props.PointerProperty(type=SceneProperties)
+    bpy.app.handlers.load_post.append(initialize_file_name)
+    bpy.app.handlers.save_post.append(initialize_file_name)
 
 
 def unregister():
     del bpy.types.Scene.clay
-    bpy.types.TOPBAR_MT_file.remove(open_export_modal)
-    bpy.utils.unregister_class(ClayExportModalOperator)
+    # bpy.types.TOPBAR_MT_file.remove(open_export_modal)
+    bpy.utils.unregister_class(ExportOperator)
     bpy.utils.unregister_class(ClayPanel)
+    bpy.utils.unregister_class(SceneProperties)
     bpy.utils.unregister_class(Preferences)
+    bpy.app.handlers.load_post.remove(initialize_file_name)
+    bpy.app.handlers.save_post.remove(initialize_file_name)
 
 
 if __name__ == "__main__":
