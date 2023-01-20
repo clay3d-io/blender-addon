b42a2f3fb52e7f373beeba7f37e5a0487f285eb9
Jake Lazaroff <jake@clay3d.io>
2023-01-20 12:01:38 -0500

    Export from side panel

--- clay/__init__.py
+++ clay/__init__.py
@@ -17,7 +17,6 @@ class Preferences(bpy.types.AddonPreferences):
     api_key: bpy.props.StringProperty(
         name="API Key",
         default="",
-        maxlen=1024,
     )
 
     def draw(self, context):
@@ -29,7 +28,6 @@ class SceneProperties(bpy.types.PropertyGroup):
     file_name: bpy.props.StringProperty(
         name="File name",
         default="",
-        maxlen=1024,
     )
 
 
@@ -39,28 +37,33 @@ class ExportOperator(bpy.types.Operator):
 
     def execute(self, context):
         clay = context.scene.clay
+        prefs = context.preferences.addons[__name__].preferences
 
         file_name = bpy.path.ensure_ext(clay.file_name or "Untitled", ".glb")
-        print(file_name)
 
-        # with tempfile.TemporaryDirectory() as folder:
-        #     filepath = os.path.join(folder, filename)
-        #     bpy.ops.export_scene.gltf(filepath=filepath, export_format="GLB")
+        with tempfile.TemporaryDirectory() as folder:
+            filepath = os.path.join(folder, file_name)
+            bpy.ops.export_scene.gltf(filepath=filepath, export_format="GLB")
 
-        #     with open(filepath, mode="rb") as file:
-        #         url = "https://api.localhost.clay3d.io/v1/workspaces/e816747e-81ca-4788-bf92-3b9a56215c0b/files"
-        #         r = requests.post(
-        #             url,
-        #             verify=False,
-        #             headers={"authorization": "Bearer " + api_token},
-        #             files={filename: file},
-        #         )
-        #         print(r.text)
+            with open(filepath, mode="rb") as file:
+                url = "https://api.localhost.clay3d.io/v1/workspaces/e816747e-81ca-4788-bf92-3b9a56215c0b/files"
+                r = requests.post(
+                    url,
+                    verify=False,
+                    headers={"authorization": "Bearer " + prefs.api_key},
+                    files={file_name: file},
+                )
+
+                if r.status_code != 200:
+                    try:
+                        json = r.json()
+                        self.report({"ERROR"}, json["message"])
+                    except Exception as e:
+                        self.report({"DEBUG"}, str(e))
+                        message = "Couldn't export file to Clay. Please contact support@clay3d.io."
+                        self.report({"ERROR"}, message)
+                    return {"CANCELLED"}
         return {"FINISHED"}
-
-    # def invoke(self, context, event):
-    #     self.filename = bpy.path.display_name(context.blend_data.filepath, has_ext=True)
-    #     return context.window_manager.invoke_props_dialog(self)
 
 
 class ClayPanel(bpy.types.Panel):
@@ -73,14 +76,8 @@ class ClayPanel(bpy.types.Panel):
     file_name: bpy.props.StringProperty(name="Filename")
 
     def draw(self, context):
-        # prefs = context.preferences.addons[__name__].preferences
-        # self.layout.label(text="Export")
         self.layout.prop(context.scene.clay, "file_name")
         self.layout.operator(ExportOperator.bl_idname, text="Export to Clay")
-
-
-# def open_export_modal(self, context):
-#     self.layout.operator(ClayExportModalOperator.bl_idname, text="Export to Clay")
 
 
 @bpy.app.handlers.persistent
@@ -97,7 +94,6 @@ def register():
     bpy.utils.register_class(SceneProperties)
     bpy.utils.register_class(ClayPanel)
     bpy.utils.register_class(ExportOperator)
-    # bpy.types.TOPBAR_MT_file.append(open_export_modal)
     bpy.types.Scene.clay = bpy.props.PointerProperty(type=SceneProperties)
     bpy.app.handlers.load_post.append(initialize_file_name)
     bpy.app.handlers.save_post.append(initialize_file_name)
@@ -105,7 +101,6 @@ def register():
 
 def unregister():
     del bpy.types.Scene.clay
-    # bpy.types.TOPBAR_MT_file.remove(open_export_modal)
     bpy.utils.unregister_class(ExportOperator)
     bpy.utils.unregister_class(ClayPanel)
     bpy.utils.unregister_class(SceneProperties)
